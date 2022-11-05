6b796a168dc1583325216faa33f69e881b7c147e
Martinson Tetteh <75953177+dojjo-corp@users.noreply.github.com>
2022-11-05 14:21:50 +0000

    Add files via upload
    Day 1 out of 100

---
+++ day_1_project.py
@@ -0,0 +1,9 @@
+print("Welcome to Dojjo's band name generator!")
+#prompt the user for the city they grew up in
+print("What is the name of the city you grew up in?")
+city = input()
+#prompt user for the name of their pet
+print("What is the name of your pet?")
+pet = input()
+
+print("Your band name could be " + city + " " + pet)
---
+++ swap_variables.py
@@ -0,0 +1,12 @@
+#switching the values of variables in python
+
+a = input('a: ')
+b = input('b: ')
+
+#your code goes below this line
+temp = a
+a = b
+b = temp
+
+print("a = " + a)
+print("b = " + b)
